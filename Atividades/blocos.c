dfafb169b487fca9fc54c64061de4dbe8bc524ac
Júlio César <ninguem26@users.noreply.github.com>
2018-01-31 10:06:23 -0300

    Create blocos.c

---
+++ Atividades/blocos.c
@@ -0,0 +1,194 @@
+
+/* Copyright (c) Mark J. Kilgard, 1994. */
+
+/* This program is freely distributable without licensing fees
+and is provided without guarantee or warrantee expressed or
+implied. This program is -not- in the public domain. */
+
+/* blender renders two spinning icosahedrons (red and green).
+The blending factors for the two icosahedrons vary sinusoidally
+and slightly out of phase.  blender also renders two lines of
+text in a stroke font: one line antialiased, the other not.  */
+
+#include <GL/glut.h>
+#include <stdio.h>
+#include <math.h>
+
+float h0 = 0;
+float h1 = 0;
+float h2 = 0;
+float h3 = 0;
+float h4 = 0;
+float speed = 1;
+
+void timerUp(int value);
+void timerDown(int value);
+
+// Função callback chamada para fazer o desenho
+void draw(void)
+{
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+// Limpa a janela de visualização com a cor de fundo especificada
+    glClear(GL_COLOR_BUFFER_BIT);
+
+// Desenha um quadrado preenchido com a cor corrente
+    glBegin(GL_QUADS);
+        glColor3f(1.0f, 1.0f, 1.0f);
+        glVertex2i(0, h0 + 50);
+        glVertex2i(0, h0 + 0);
+        glVertex2i(50, h0 + 0);
+        glVertex2i(50, h0 + 50);
+    glEnd();
+
+    glBegin(GL_QUADS);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2i(50, h1 + 50);
+        glVertex2i(50, h1 + 0);
+        glVertex2i(100, h1 + 0);
+        glVertex2i(100, h1 + 50);
+    glEnd();
+
+    glBegin(GL_QUADS);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex2i(100, h2 + 50);
+        glVertex2i(100, h2 + 0);
+        glVertex2i(150, h2 + 0);
+        glVertex2i(150, h2 + 50);
+    glEnd();
+
+    glBegin(GL_QUADS);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2i(150, h3 + 50);
+        glVertex2i(150, h3 + 0);
+        glVertex2i(200, h3 + 0);
+        glVertex2i(200, h3 + 50);
+    glEnd();
+
+    glBegin(GL_QUADS);
+        glColor3f(1.0f, 0.0f, 1.0f);
+        glVertex2i(200, h4 + 50);
+        glVertex2i(200, h4 + 0);
+        glVertex2i(250, h4 + 0);
+        glVertex2i(250, h4 + 50);
+    glEnd();
+
+// Executa os comandos OpenGL
+    glFlush();
+}
+
+// Inicializa parâmetros de rendering
+void init (void)
+{
+// Define a cor de fundo da janela de visualização como preta
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+// Função callback chamada quando o tamanho da janela é alterado
+void changeWindowSize(GLsizei w, GLsizei h)
+{
+           // Evita a divisao por zero
+    if(h == 0) h = 1;
+
+           // Especifica as dimensões da Viewport
+    glViewport(0, 0, w, h);
+
+           // Inicializa o sistema de coordenadas
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    // Estabelece a janela de seleção (left, right, bottom, top)
+    if (w <= h)
+        gluOrtho2D (0.0f, 250.0f, 0.0f, 250.0f*h/w);
+    else
+        gluOrtho2D (0.0f, 250.0f*w/h, 0.0f, 250.0f);
+}
+
+void timerDown(int value)
+{
+    int up = 0;
+
+    if(h0 > 0 && h1 > 0 && h2 > 0 && h3 > 0 && h4 > 0) {
+        h0 -= speed;
+        h1 -= speed;
+        h2 -= speed;
+        h3 -= speed;
+        h4 -= speed;
+    } else {
+        h0 = 0;
+        h1 = 0;
+        h2 = 0;
+        h3 = 0;
+        h4 = 0;
+        up = 1;
+    }
+
+    // Redesenha o quadrado com as novas coordenadas
+    glutPostRedisplay();
+    if(up) {
+        glutTimerFunc(33,timerUp, 1);
+    } else {
+        glutTimerFunc(33,timerDown, 1);
+    }
+}
+
+// Função callback chamada pela GLUT a cada intervalo de tempo
+// (a window não está sendo redimensionada ou movida)
+void timerUp(int value)
+{
+    int down = 0;
+
+    if(h0 < 50) {
+        h0 += speed;
+    } else {
+        h0 = 50;
+
+        if(h1 < 50) {
+            h1 += speed;
+        } else {
+            h1 = 50;
+            if(h2 < 50) {
+                h2 += speed;
+            } else {
+                h2 = 50;
+
+                if(h3 < 50) {
+                    h3 += speed;
+                } else {
+                    h3 = 50;
+
+                    if(h4 < 50) {
+                        h4 += speed;
+                    } else {
+                        h4 = 50;
+                        down = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    // Redesenha o quadrado com as novas coordenadas
+    glutPostRedisplay();
+    if(!down) {
+        glutTimerFunc(33,timerUp, 1);
+    } else {
+        glutTimerFunc(33,timerDown, 1);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(400,400);
+    glutInitWindowPosition(0,0);
+    glutCreateWindow("Blocos");
+    glutDisplayFunc(draw);
+    glutReshapeFunc(changeWindowSize);
+    glutTimerFunc(33, timerUp, 1);
+    init();
+    glutMainLoop();
+return 0;             /* ANSI C requires main to return int. */
+}
